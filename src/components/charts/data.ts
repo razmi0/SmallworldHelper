9a0c6f99795b948331db96160d5c103ebb51f787
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-10-29 22:30:09 +0100

    feat : chart>defined colors arr & new option data

--- src/components/charts/data.ts
+++ src/components/charts/data.ts
@@ -1,10 +1,17 @@
-import { ChartData, ChartOptions } from "chart.js";
-
-const labels = Array.from({ length: Math.floor(Math.random() * 30) }, (_, i) => i.toString());
-console.log(labels);
+import { ChartOptions } from "chart.js";
 
 export const options: ChartOptions<"line"> = {
   responsive: true,
+  elements: {
+    point: {
+      radius: 0,
+    },
+    line: {
+      tension: 0.4,
+      borderWidth: 3,
+    },
+  },
+  maintainAspectRatio: false,
   scales: {
     x: {
       display: false,
@@ -13,30 +20,49 @@ export const options: ChartOptions<"line"> = {
       },
     },
     y: {
+      display: false,
+
       grid: {
         display: false,
       },
     },
   },
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+
   plugins: {
     legend: {
       display: false,
     },
+    tooltip: {
+      callbacks: {
+        title: () => "",
+      },
+      bodyFont: {
+        size: 16,
+      },
+    },
   },
 };
 
-export const data: ChartData<"line"> = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map((_, i) => i + Math.random() * 10),
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map((_, i) => i + Math.random() * 10),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
+export const playerColors = [
+  "#f94144",
+  "#43aa8b",
+  "#4cc9f0",
+  "#3a0ca3",
+  "#f9c74f",
+  "#4d908e",
+  "#4361ee",
+  "#f3722c",
+  "#f72585",
+  "#4895ef",
+  "#560bad",
+  "#277da1",
+  "#3f37c9",
+  "#f8961e",
+  "#577590",
+  "#b5179e",
+  "#90be6d",
+];
