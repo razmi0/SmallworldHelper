2b5b9058ae54f7974a0b453c56e086d5ead1a815
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-10-30 02:52:16 +0100

    feat : Heading component is unused

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,24 +1,16 @@
 export const Heading = ({ name, victoryPtn }: { name: string; victoryPtn: number }) => {
   return (
-    <span className="heading">
-      <style>
-        {`
-        .heading {
-            font-weight: bold;
-            font-size: 30px;
-            transform: translate(-30px);
-        }
-        .heading-img {
-            height: 20px;
-            width: 20px;
-            margin-right: 10px;
-        }
-        `}
-      </style>
-      <img className="heading-img" src="/chevron-right.png" alt="title name icon " />
+    <span
+      style={{
+        fontWeight: "bold",
+        fontSize: "30px",
+      }}
+    >
       {name} : {victoryPtn}
     </span>
   );
 };
 
 export default Heading;
+
+//             -webkit-text-fill-color: transparent; -webkit-background-clip: text;
