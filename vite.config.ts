324e9b518579554823b031fcd1fc5dcc0717ed46
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-10-31 00:02:51 +0100

    aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@Icons": path.resolve(__dirname, "./src/components/icons"),
+      "@Charts": path.resolve(__dirname, "./src/components/charts"),
+      "@Components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
