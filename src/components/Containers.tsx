e1315479ca58bc2b4afc2fbc378ea194faad1614
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-02 18:27:45 +0100

    Add ChartContainer component to Containers.tsx

--- src/components/Containers.tsx
+++ src/components/Containers.tsx
@@ -7,3 +7,18 @@ type FlexProps = {
 export const Flex = ({ children, sx }: FlexProps) => {
   return <div style={{ ...sx, display: "flex" }}>{children}</div>;
 };
+
+export const ChartContainer = ({ children }: { children: ReactNode }) => {
+  return (
+    <figure
+      style={{
+        maxHeight: "250px",
+        marginTop: "0px",
+        marginBottom: "0px",
+      }}
+      className="chart-ctn"
+    >
+      {children}
+    </figure>
+  );
+};
