90ca8a0e3a536f8479505c70580806c981853193
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-07 18:19:03 +0100

    Add helper functions for usePlayer hook reducer

---
+++ src/hooks/helpers.ts
@@ -0,0 +1,289 @@
+import { playerColors } from "src/components/icons/data";
+import { BarData, LineData, Player, PieData } from "src/types";
+import { addOpacityToHex, findMaxNbrTurns, getRandomColor } from "src/utils";
+
+// HELPERS FUNCTIONS FOR ADD_PLAYER
+//--
+
+export const buildBaseStats = (name: string, startScore: number, id: number) => {
+  return {
+    id: id,
+    name: name.charAt(0).toUpperCase() + name.slice(1),
+    victoryPtn: startScore,
+    history: [startScore],
+    addedScores: [startScore],
+    rankChange: 0,
+    color: playerColors[id] ?? getRandomColor(),
+    max: startScore,
+    min: startScore,
+    avg: startScore,
+    sum: startScore,
+  };
+};
+
+export const buildNewLines = (lines: LineData, newPlayer: Player) => {
+  const { datasets, labels } = lines;
+  return {
+    labels: labels,
+    datasets: [
+      ...datasets,
+      {
+        label: newPlayer.name,
+        data: [...newPlayer.history],
+        backgroundColor: newPlayer.color,
+        borderColor: newPlayer.color,
+      },
+    ],
+  };
+};
+
+export const buildNewBars = (bars: BarData, newPlayer: Player) => {
+  const { datasets, labels } = bars;
+  const clonedDatasets = datasets.map((dataset) => ({
+    ...dataset,
+    data: [...dataset.data, newPlayer.victoryPtn],
+    borderColor: [...dataset.borderColor, newPlayer.color],
+    backgroundColor: [...dataset.backgroundColor, addOpacityToHex(newPlayer.color, 0.8)],
+  }));
+  return {
+    labels: [...labels, newPlayer.name],
+    datasets: clonedDatasets,
+  };
+};
+
+export const buildNewPies = (pies: PieData, newPlayer: Player) => {
+  const { datasets, labels } = pies;
+  return {
+    labels: [...labels, newPlayer.name],
+    datasets: [
+      {
+        ...datasets[0],
+        data: [...datasets[0].data, newPlayer.victoryPtn],
+        backgroundColor: [...datasets[0].backgroundColor, addOpacityToHex(newPlayer.color, 0.8)],
+        borderColor: [...datasets[0].borderColor, newPlayer.color],
+      },
+    ],
+  };
+};
+
+// HELPERS FUNCTIONS FOR UPDATE_SCORE
+//--
+const updatePlayerStats = (player: Player, newScore: number) => {
+  const updatedPlayer = {
+    ...player,
+    history: [...player.history, newScore],
+    addedScores: [...player.addedScores, newScore],
+    victoryPtn: player.victoryPtn + newScore,
+    max: Math.max(player.max, newScore),
+    min: Math.min(player.min, newScore),
+    avg: Math.round((player.sum + newScore) / (player.addedScores.length + 1)),
+    sum: player.sum + newScore,
+  };
+
+  return updatedPlayer;
+};
+
+export const updatePlayersStats = (players: Player[], newScore: number, id: number) => {
+  const playerIndex = players.findIndex((p) => p.id === id);
+  if (playerIndex === -1) {
+    throw new Error("Player not found");
+  }
+  const updatedPlayer = updatePlayerStats(players[playerIndex], newScore);
+  const updatedPlayers = [
+    ...players.slice(0, playerIndex),
+    updatedPlayer,
+    ...players.slice(playerIndex + 1),
+  ];
+
+  return { updatedPlayer, updatedPlayers };
+};
+
+export const updateLines = (
+  lines: LineData,
+  updatedPlayer: Player,
+  players: Player[]
+): LineData => {
+  const newLabels =
+    findMaxNbrTurns(players) > lines.labels.length
+      ? [...lines.labels, (lines.labels.length + 1).toString()]
+      : [...lines.labels];
+  const newDatasets = lines.datasets.map((dataset) => {
+    if (dataset.label === updatedPlayer.name) {
+      return {
+        ...dataset,
+        data: [...dataset.data, updatedPlayer.victoryPtn],
+      };
+    }
+    return dataset;
+  });
+  return {
+    labels: newLabels,
+    datasets: newDatasets,
+  };
+};
+
+export const updateBars = (bars: BarData, updatedPlayer: Player): BarData => {
+  const index = bars.labels.findIndex((label) => label === updatedPlayer.name);
+  const newDatasets = bars.datasets.map((dataset, i) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData[index] =
+        i === 0 ? updatedPlayer.max : i === 1 ? updatedPlayer.min : updatedPlayer.avg;
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: [...bars.labels], // This spread is not necessary labels are not being mutated
+    datasets: newDatasets,
+  };
+};
+
+export const updatePies = (pies: PieData, updatedPlayer: Player): PieData => {
+  const index = pies.labels.findIndex((label) => label === updatedPlayer.name);
+  const newDatasets = pies.datasets.map((dataset) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData[index] = updatedPlayer.victoryPtn;
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: [...pies.labels], // This spread is not necessary labels are not being mutated
+    datasets: newDatasets,
+  };
+};
+
+// HELPERS FUNCTIONS FOR RESET_PLAYER
+//--
+export const resetPlayersStats = (players: Player[], id: Player["id"]) => {
+  const newPlayers = players.map((player) => {
+    return player.id === id ? resetPlayerStats(player) : player;
+  });
+  const index = newPlayers.findIndex((player) => player.id === id);
+  if (index === -1) {
+    throw new Error("Player not found");
+  }
+  return {
+    newPlayer: newPlayers[index],
+    newPlayers: newPlayers,
+  };
+};
+
+const resetPlayerStats = (player: Player) => {
+  return {
+    ...player,
+    history: [...player.history, 0],
+    addedScores: [...player.addedScores, 0],
+    victoryPtn: 0,
+  };
+};
+
+export const resetLine = (lines: LineData, name: Player["name"], players: Player[]): LineData => {
+  const newDatasets = lines.datasets.map((dataset) => {
+    return dataset.label === name ? { ...dataset, data: [...dataset.data, 0] } : dataset;
+  });
+  const newLabels =
+    findMaxNbrTurns(players) > lines.labels.length
+      ? [...lines.labels, (lines.labels.length + 1).toString()]
+      : lines.labels;
+  return {
+    labels: newLabels,
+    datasets: newDatasets,
+  };
+};
+
+export const resetBar = (bars: BarData, name: Player["name"]): BarData => {
+  const index = bars.labels.findIndex((label) => label === name);
+  const newDatasets = bars.datasets.map((dataset) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData[index] = 0;
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: bars.labels,
+    datasets: newDatasets,
+  };
+};
+
+export const resetPie = (pies: PieData, name: Player["name"]): PieData => {
+  const index = pies.labels.findIndex((label) => label === name);
+  const newDatasets = pies.datasets.map((dataset) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData[index] = 0;
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: pies.labels,
+    datasets: newDatasets,
+  };
+};
+
+// HELPERS FUNCTIONS FOR REMOVE_PLAYER
+//--
+export const removePlayer = (players: Player[], id: Player["id"]) => {
+  const index = players.findIndex((player) => player.id === id);
+  return {
+    rmPlayers: players.filter((player) => player.id !== id),
+    rmPlayer: players[index],
+  };
+};
+
+export const removeLine = (lines: LineData, name: Player["name"]): LineData => {
+  const newDatasets = lines.datasets.filter((dataset) => dataset.label !== name);
+  return {
+    labels: [...lines.labels],
+    datasets: newDatasets,
+  };
+};
+
+export const removeBar = (bars: BarData, name: Player["name"]): BarData => {
+  const index = bars.labels.findIndex((label) => label === name);
+  const newDatasets = bars.datasets.map((dataset) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData.splice(index, 1);
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: bars.labels.filter((label) => label !== name),
+    datasets: newDatasets,
+  };
+};
+
+export const removePie = (pies: PieData, name: Player["name"]): PieData => {
+  const index = pies.labels.findIndex((label) => label === name);
+  const newDatasets = pies.datasets.map((dataset) => {
+    const newData = [...dataset.data];
+    if (index !== -1) {
+      newData.splice(index, 1);
+    }
+    return {
+      ...dataset,
+      data: newData,
+    };
+  });
+  return {
+    labels: pies.labels.filter((label) => label !== name),
+    datasets: newDatasets,
+  };
+};
