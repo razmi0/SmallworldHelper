e7b52f4a790937ba60da8b925c3feee70b258ccc
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-08 16:10:31 +0100

    Add useToggle hook for managing toggle states

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,63 @@
+import { useCallback, useReducer } from "react";
+
+type ToggleState = {
+  isNavOpen: boolean;
+  isAddPlayerOpen: boolean;
+  isChartsOpen: boolean;
+  isScoreHidden: boolean;
+};
+
+type ToggleActions =
+  | { type: "TOGGLE_HIDE_SCORE" }
+  | { type: "TOGGLE_OPEN_ADD_PLAYER" }
+  | { type: "TOGGLE_OPEN_NAV" }
+  | { type: "TOGGLE_OPEN_CHARTS" };
+
+// INITIAL STATES
+//--
+const initialToggleStates = {
+  isNavOpen: false,
+  isAddPlayerOpen: false,
+  isChartsOpen: false,
+  isScoreHidden: false,
+};
+
+// REDUCER
+//--
+const toggleReducer = (state: ToggleState, action: ToggleActions): ToggleState => {
+  switch (action.type) {
+    case "TOGGLE_HIDE_SCORE": {
+      return { ...state, isScoreHidden: !state.isScoreHidden };
+    }
+
+    case "TOGGLE_OPEN_ADD_PLAYER": {
+      return { ...state, isAddPlayerOpen: !state.isAddPlayerOpen };
+    }
+    case "TOGGLE_OPEN_NAV": {
+      return { ...state, isNavOpen: !state.isNavOpen };
+    }
+    case "TOGGLE_OPEN_CHARTS": {
+      return { ...state, isChartsOpen: !state.isChartsOpen };
+    }
+    default:
+      return state;
+  }
+};
+
+// HOOK
+//--
+export const useToggle = () => {
+  const [state, dispatch] = useReducer(toggleReducer, {
+    ...initialToggleStates,
+  });
+  return {
+    isNavOpen: state.isNavOpen,
+    isAddPlayerOpen: state.isAddPlayerOpen,
+    isChartsOpen: state.isChartsOpen,
+    isScoreHidden: state.isScoreHidden,
+    toggleHideScore: useCallback(() => dispatch({ type: "TOGGLE_HIDE_SCORE" }), []),
+    toggleOpenAddPlayer: useCallback(() => dispatch({ type: "TOGGLE_OPEN_ADD_PLAYER" }), []),
+    toggleOpenNav: useCallback(() => dispatch({ type: "TOGGLE_OPEN_NAV" }), []),
+    toggleOpenCharts: useCallback(() => dispatch({ type: "TOGGLE_OPEN_CHARTS" }), []),
+  };
+};
