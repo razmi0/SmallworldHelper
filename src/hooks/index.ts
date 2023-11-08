9b7add3e28b0c7dfe2fea49c4e0e5ce9b6bc1933
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-08 16:09:53 +0100

    Module for hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,9 @@
+import { usePlayer } from "./usePlayer";
+import { useIntermediateState } from "./useIntermediateState";
+import { useTheme } from "./useTheme";
+import { useToggle } from "./useToggle";
+
+// HERE CAN GO THE STORE LATER
+// ...
+
+export { usePlayer, useIntermediateState, useTheme, useToggle };
