532f20a3e52e160a826d5971e453c196748a7388
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-09 18:47:52 +0100

    Fix import path for ThemeProvider in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./hooks/context/useTheme.tsx";
+import { ThemeProvider } from "./hooks/context/theme/useTheme.tsx";
 import { IntermediateProvider } from "./hooks/context/useIntermediateState.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
