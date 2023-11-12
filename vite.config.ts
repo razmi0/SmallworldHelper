3c8c374e7b43a01134c23ab65499546f15d66e1b
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-12 13:45:03 +0100

    Delete unused test files and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+// import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@Icons": path.resolve(__dirname, "./src/components/icons"),
-      "@Charts": path.resolve(__dirname, "./src/components/charts"),
-      "@Components": path.resolve(__dirname, "./src/components"),
-    },
-  },
   test: {
+    setupFiles: ["src/test/vitest.setup.ts"],
     includeSource: ["src/**/*.{js,ts,tsx}"],
     environment: "jsdom",
   },
