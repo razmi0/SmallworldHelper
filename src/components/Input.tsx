ba589e07013ca98d2ae3501cfb2bfc199f5ce76a
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-14 11:44:21 +0100

    Commented out InputButton + related & fowardRef on  the rest

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,126 +1,105 @@
-import {
-  InputHTMLAttributes,
-  KeyboardEvent,
-  ReactNode,
-  FC,
-  CSSProperties,
-  MouseEventHandler,
-} from "react";
+import { KeyboardEvent, ReactNode, CSSProperties, forwardRef, InputHTMLAttributes } from "react";
 import { add4dToHex } from "../utils";
 import { KeyboardNavigationIdType } from "../types";
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   datatype?: KeyboardNavigationIdType;
-  labelText?: string;
+  label?: string;
   subjectId: string;
   onEnter?: (e: KeyboardEvent<HTMLInputElement>) => void;
   children?: ReactNode;
   sx?: CSSProperties;
   theme?: "light" | "dark";
-  onFocus?: () => void;
-  onBlur?: () => void;
   color?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   value?: number | string;
 }
 
-interface InputButtonProps extends InputProps {
-  btnText: string;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-}
+// interface InputButtonProps extends InputProps {
+//   btnText: string;
+//   onClick?: MouseEventHandler<HTMLButtonElement>;
+// }
 
-export const Input: FC<InputProps & InputHTMLAttributes<HTMLInputElement>> = ({
-  onEnter = () => {},
-  children,
-  subjectId,
-  labelText,
-  ...inputProps
-}: InputProps) => {
-  const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (onEnter && e.key === "Enter") {
-      onEnter(e);
-    }
-  };
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ onEnter, children, subjectId, label: labelText, ...rest }, ref) => {
+    const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (onEnter && e.key === "Enter") {
+        onEnter(e);
+      }
+    };
 
-  return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <label style={{ marginRight: "3px" }} htmlFor={subjectId}>
-        {labelText}
-      </label>
-      {children}
-      <input
-        {...inputProps}
-        minLength={1}
-        type="text"
-        name={subjectId}
-        id={subjectId}
-        onKeyUp={(e) => handleEnter(e)}
-      />
-    </div>
-  );
-};
+    return (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <label style={{ marginRight: "3px" }} htmlFor={subjectId}>
+          {labelText}
+        </label>
+        {children}
+        <input
+          ref={ref}
+          {...rest}
+          minLength={1}
+          type="text"
+          name={subjectId}
+          id={subjectId}
+          onKeyUp={(e) => handleEnter(e)}
+        />
+      </div>
+    );
+  }
+);
+// export const InputButton: FC<InputButtonProps & InputHTMLAttributes<HTMLInputElement>> = ({
+//   onEnter = () => {},
+//   children,
+//   subjectId,
+//   label : labelText,
+//   btnText,
+//   onClick = () => {},
+//   value,
+// }: InputButtonProps) => {
+//   const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+//     if (onEnter && e.key === "Enter") {
+//       onEnter(e);
+//     }
+//   };
 
-export const InputButton: FC<InputButtonProps & InputHTMLAttributes<HTMLInputElement>> = ({
-  onEnter = () => {},
-  children,
-  subjectId,
-  labelText,
-  btnText,
-  onClick = () => {},
-  value,
-  onChange = () => {},
-}: InputButtonProps) => {
-  const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (onEnter && e.key === "Enter") {
-      onEnter(e);
-    }
-  };
+//   return (
+//     <div style={{ display: "flex", alignItems: "center" }}>
+//       <label style={{ marginRight: "3px" }} htmlFor={subjectId}>
+//         {labelText}
+//       </label>
+//       {children}
+//       <input
+//         onChange={onChange}
+//         value={value}
+//         minLength={1}
+//         type="text"
+//         name={subjectId}
+//         id={subjectId}
+//         onKeyUp={(e) => handleEnter(e)}
+//       />
+//       <button
+//         onClick={onClick}
+//         style={{
+//           transform: "translate(-5px, 25px)",
+//           zIndex: 1,
+//         }}
+//       >
+//         {btnText}
+//       </button>
+//     </div>
+//   );
+// };
 
-  return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <label style={{ marginRight: "3px" }} htmlFor={subjectId}>
-        {labelText}
-      </label>
-      {children}
-      <input
-        onChange={onChange}
-        value={value}
-        minLength={1}
-        type="text"
-        name={subjectId}
-        id={subjectId}
-        onKeyUp={(e) => handleEnter(e)}
-      />
-      <button
-        onClick={onClick}
-        style={{
-          transform: "translate(-5px, 25px)",
-          zIndex: 1,
-        }}
-      >
-        {btnText}
-      </button>
-    </div>
-  );
-};
-
-export const SoftInput = ({
-  subjectId,
-  sx,
-  // isFocus,
-  // onFocus,
-  // onBlur,
-  color,
-  onChange,
-  onKeyUp,
-  datatype,
-  value,
-}: InputProps) => {
-  return (
-    <div className="form__group">
-      <style>
-        {`
+export const SoftInput = forwardRef<HTMLInputElement, InputProps>(
+  ({ subjectId, sx, color, datatype, value, onEnter, label = "Add score", ...rest }, ref) => {
+    const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (onEnter && e.key === "Enter") {
+        onEnter(e);
+      }
+    };
+    return (
+      <div className="form__group">
+        <style>
+          {`
         .form__field--${subjectId}:focus {
           border-image: linear-gradient(to right, ${color}, ${add4dToHex(color)});
           border-image-slice: 1;
@@ -130,24 +109,24 @@ export const SoftInput = ({
           color: ${color};
         }
       `}
-      </style>
-      <input
-        datatype={datatype}
-        // onFocus={onFocus}
-        // onBlur={onBlur}
-        className={`form__field--${subjectId} form__field--commun`}
-        placeholder="Score"
-        id={subjectId}
-        style={sx}
-        type="text"
-        onChange={onChange}
-        onKeyUp={onKeyUp}
-        value={value}
-        autoComplete="off"
-      />
-      <label htmlFor={subjectId} className="form__label">
-        Add score
-      </label>
-    </div>
-  );
-};
+        </style>
+        <input
+          ref={ref}
+          datatype={datatype}
+          onKeyUp={(e) => handleEnter(e)}
+          className={`form__field--${subjectId} form__field--commun`}
+          id={subjectId}
+          style={sx}
+          type="text"
+          placeholder={label}
+          value={value}
+          autoComplete="off"
+          {...rest}
+        />
+        <label htmlFor={subjectId} className="form__label">
+          {label}
+        </label>
+      </div>
+    );
+  }
+);
