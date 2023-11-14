7e74a491b523fd909e40325a4f631945c7564d3c
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-14 14:24:21 +0100

    Change --light to a more gold like color

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -46,24 +46,24 @@ export type PieProps = {
 
 export const Line = ({ data, options, theme = "dark" }: LineProps) => {
   if (options.plugins?.tooltip) {
-    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffffde";
-    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffffde" : "#242424";
+    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffde";
+    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffde" : "#242424";
   }
   return <ChartLine data={data} options={options} />;
 };
 
 export const Pie = ({ data, options, theme = "dark" }: PieProps) => {
   if (options.plugins?.tooltip) {
-    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffffde";
-    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffffde" : "#242424";
+    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffde";
+    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffde" : "#242424";
   }
   return <ChartPie data={data} options={options} />;
 };
 
 export const Bar = ({ data, options, theme = "dark" }: BarProps) => {
   if (options.plugins?.tooltip) {
-    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffffde";
-    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffffde" : "#242424";
+    options.plugins.tooltip.backgroundColor = theme === "dark" ? "#242424" : "#ffffde";
+    options.plugins.tooltip.bodyColor = theme === "dark" ? "#ffffde" : "#242424";
   }
   return <ChartBar options={options} data={data} />;
 };
