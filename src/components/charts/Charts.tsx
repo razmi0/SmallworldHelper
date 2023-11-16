aa07741aede954893942f38747ce422ac42c0fd5
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-16 21:35:49 +0100

    Add focus on player functionality to charts

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -13,6 +13,9 @@ import {
   BarElement,
 } from "chart.js";
 import { Line as ChartLine, Pie as ChartPie, Bar as ChartBar } from "react-chartjs-2";
+import { useIntermediate } from "../../hooks";
+import { addOpacityToHex } from "../../utils";
+import { useEffect, useState } from "react";
 
 ChartJS.register(
   CategoryScale,
@@ -44,14 +47,90 @@ export type PieProps = {
   theme?: "light" | "dark";
 };
 
+export const focusOnPlayerLine = (isFocus: boolean[], data: ChartData<"line">) => {
+  const playerFocusedIndex = isFocus.findIndex((isF) => isF);
+  if (playerFocusedIndex === -1 || !data) {
+    return data;
+  }
+  const newDatasets = data.datasets.map((dataset, i) => {
+    const playerColor = dataset.backgroundColor as string;
+    const playerBorderColor = dataset.borderColor as string;
+    if (i === playerFocusedIndex) {
+      return {
+        ...dataset,
+        backgroundColor: addOpacityToHex(playerColor),
+        borderColor: addOpacityToHex(playerBorderColor),
+      };
+    } else {
+      return {
+        ...dataset,
+        backgroundColor: addOpacityToHex(playerColor, 0.1),
+        borderColor: addOpacityToHex(playerBorderColor, 0.1),
+      };
+    }
+  });
+  return { ...data, datasets: newDatasets };
+};
+
+export const focusOnPlayerPie = (isFocus: boolean[], data: ChartData<"pie">) => {
+  const playerFocusedIndex = isFocus.findIndex((isF) => isF);
+  if (playerFocusedIndex === -1 || data.datasets.length === 0) {
+    return data;
+  }
+  const colors = data.datasets[0].backgroundColor as string[];
+  const newColors = colors.map((color, i) =>
+    i !== playerFocusedIndex ? addOpacityToHex(color, 0.1) : color
+  );
+  const newDatasets = data.datasets.map((dataset) => {
+    return { ...dataset, backgroundColor: newColors };
+  });
+  return { ...data, datasets: newDatasets };
+};
+
+export const focusOnPlayerBar = (isFocus: boolean[], data: ChartData<"bar">) => {
+  const playerFocusedIndex = isFocus.findIndex((isF) => isF);
+  if (playerFocusedIndex === -1 || data.datasets.length === 0) {
+    return data;
+  }
+  const newDatasets = data.datasets.map((dataset) => {
+    const playersColors = dataset.backgroundColor as string[];
+    const newColors = playersColors.map((color, j) =>
+      j !== playerFocusedIndex ? addOpacityToHex(color, 0.1) : color
+    );
+    return { ...dataset, backgroundColor: newColors, borderColor: newColors };
+  });
+  return { ...data, datasets: newDatasets };
+};
+
 export const Line = ({ data, options /* theme = "dark" */ }: LineProps) => {
-  return <ChartLine data={data} options={options} />;
+  const { isFocus } = useIntermediate();
+  const [treatedData, setTreatedData] = useState<ChartData<"line">>(data);
+
+  useEffect(() => {
+    setTreatedData(focusOnPlayerLine(isFocus, data));
+  }, [isFocus]);
+
+  return <ChartLine data={treatedData} options={options} />;
 };
 
 export const Pie = ({ data, options /* theme = "dark" */ }: PieProps) => {
-  return <ChartPie data={data} options={options} />;
+  const { isFocus } = useIntermediate();
+  const [treatedData, setTreatedData] = useState<ChartData<"pie">>(data);
+
+  useEffect(() => {
+    setTreatedData(focusOnPlayerPie(isFocus, data));
+  }, [isFocus]);
+
+  return <ChartPie data={treatedData} options={options} />;
 };
 
 export const Bar = ({ data, options /* theme = "dark" */ }: BarProps) => {
-  return <ChartBar options={options} data={data} />;
+  const { isFocus } = useIntermediate();
+  const [treatedData, setTreatedData] = useState<ChartData<"bar">>(data);
+
+  useEffect(() => {
+    setTreatedData(focusOnPlayerBar(isFocus, data));
+  }, [isFocus]);
+
+  return <ChartBar data={treatedData} options={options} />;
 };
