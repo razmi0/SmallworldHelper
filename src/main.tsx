521fd2ba8155a4692f9e9086165d6766ec5137a7
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-29 16:43:46 +0100

    Add Notification Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { NotificationProvider, IntermediateProvider, ThemeProvider } from "@Hooks";
 import App from "./App.tsx";
 import "@Styles";
-import { IntermediateProvider, ThemeProvider } from "@Hooks";
-import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <IntermediateProvider>
-        <App />
-      </IntermediateProvider>
+      <NotificationProvider>
+        <IntermediateProvider>
+          <App />
+        </IntermediateProvider>
+      </NotificationProvider>
     </ThemeProvider>
   </StrictMode>
 );
