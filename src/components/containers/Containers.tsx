2d8189b4150c55c70ac84f7f199b9d464c6816ff
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-30 16:27:42 +0100

    Add chartStyles import and update ChartContainer class names

--- src/components/containers/Containers.tsx
+++ src/components/containers/Containers.tsx
@@ -1,21 +1,26 @@
 import { ReactNode, useId } from "react";
 import styles from "./_.module.css";
+import chartStyles from "../charts/_.module.css";
 import { ContainerProps } from "@Types";
 
 export const ChartContainer = ({ children, isOpen }: { children: ReactNode; isOpen: boolean }) => {
   const childrenArr = Array.isArray(children) ? children : [children];
   const id = useId().replace(/:/g, "_");
+  const classes = " grainy lin-dark global-grainy shadow-ctn";
 
   return (
     <section className="charts-ctn">
       {isOpen &&
         children &&
         childrenArr.map((child, i) => {
+          const isDonut = child.type.name === "Doughnut";
           return (
             <figure
               id={id}
               key={i}
-              className={`grainy lin-dark global-grainy shadow-ctn ${styles["figure-ctn"]}`}
+              className={`${classes} ${styles["figure-ctn"]} ${
+                isDonut ? chartStyles["donut"] : ""
+              }`}
             >
               {child}
             </figure>
