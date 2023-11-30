16b55b863d4285d400d769f971038bdff9382da5
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-11-30 16:25:15 +0100

    Remove StrictMode from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { NotificationProvider, IntermediateProvider, ThemeProvider } from "@Hooks";
 import App from "./App.tsx";
 import "@Styles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <NotificationProvider>
-        <IntermediateProvider>
-          <App />
-        </IntermediateProvider>
-      </NotificationProvider>
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider>
+    <NotificationProvider>
+      <IntermediateProvider>
+        <App />
+      </IntermediateProvider>
+    </NotificationProvider>
+  </ThemeProvider>
 );
