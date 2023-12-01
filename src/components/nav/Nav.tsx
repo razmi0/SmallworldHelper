916cbf547e6450e0fc2955cfee709dfc763f0038
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-01 17:57:04 +0100

    Add toggle nav & styles

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { useMidAction /* useSwitchTheme */ } from "@Hooks";
+import { useClickOutside, useMidAction /* useSwitchTheme */ } from "@Hooks";
 import {
   Header,
   IconAddPlayer,
@@ -10,11 +10,12 @@ import {
   Undo,
   Redo,
   Save,
+  Menu,
 } from "@Components";
 import { withViewTransition } from "@Utils";
-import { Player, UndoRedoActions, UndoRedoStates } from "@Types";
+import { ContainerProps, Player, UndoRedoActions, UndoRedoStates } from "@Types";
 import styles from "./_.module.css";
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 
 type NavUndoRedoStates = Pick<UndoRedoStates<Player[]>, "isRedoPossible" | "isUndoPossible">;
 
@@ -23,6 +24,8 @@ type NavProps = {
   toggleOpenAddPlayer: (newState?: boolean) => void;
   toggleOpenCharts: () => void;
   toggleHideScore: () => void;
+  toggleOpenNav: () => void;
+  isNavOpen: boolean;
   isScoreHidden: boolean;
   undoRedoStates: NavUndoRedoStates;
   undoRedoActions: Omit<UndoRedoActions<Player[]>, "setState">;
@@ -34,6 +37,8 @@ export const Nav = ({
   toggleHideScore,
   toggleOpenAddPlayer,
   toggleOpenCharts,
+  toggleOpenNav,
+  isNavOpen,
   undoRedoStates,
   undoRedoActions,
   isScoreHidden,
@@ -43,73 +48,112 @@ export const Nav = ({
   const { setLoadPlayers, setSavePlayers } = storageActions;
   const { isRedoPossible, isUndoPossible } = undoRedoStates;
   const { undo, redo } = undoRedoActions;
+  const navRef = useRef<HTMLElement>(null) as MutableRefObject<HTMLElement>;
 
   useEffect(() => {
     setAllowViewTransition(true);
   }, [playerSize]);
 
+  useClickOutside(navRef, () => {
+    if (isNavOpen && navRef) withViewTransition(toggleOpenNav);
+  });
+
   return (
     <Header>
-      <nav className={styles["nav-ctn"]}>
+      <nav className={styles["nav-ctn"]} ref={navRef}>
         <IconButton
           variant="nav"
-          icon={Load}
-          iconName="load"
-          onClick={() => setLoadPlayers(true)}
+          iconName="menu"
+          icon={Menu}
+          onClick={() => withViewTransition(toggleOpenNav)}
         />
-        <IconButton
-          variant="nav"
-          icon={Save}
-          onClick={() => setSavePlayers(true)}
-          iconName="save"
-        />
-        <IconButton
-          variant="nav"
-          icon={IconAddPlayer}
-          iconName="addplayer"
-          onClick={() => withViewTransition(toggleOpenAddPlayer)}
-        />
-        <IconButton
-          variant="nav"
-          icon={Chart}
-          iconName="chart"
-          onClick={() => withViewTransition(toggleOpenCharts)}
-        />
-        <IconButton
-          variant="nav"
-          icon={isScoreHidden ? EyeClose : EyeOpen}
-          iconName="eyes"
-          onClick={toggleHideScore}
-        />
-        <IconButton
-          variant="nav"
-          icon={Undo}
-          iconName="undo"
-          onClick={() => {
-            if (!allowViewTransition) {
-              undo();
-              return;
-            }
-            withViewTransition(undo);
-            setAllowViewTransition(false);
-          }}
-          disabled={!isUndoPossible}
-        />
-        <IconButton
-          variant="nav"
-          icon={Redo}
-          iconName="redo"
-          onClick={() => {
-            if (!allowViewTransition) {
-              redo();
-              return;
-            }
-            withViewTransition(redo);
-            setAllowViewTransition(false);
-          }}
-          disabled={!isRedoPossible}
-        />
+        {isNavOpen && (
+          <ViewTransitionManager>
+            <IconButton
+              variant="nav"
+              icon={Load}
+              iconName="load"
+              onClick={() => setLoadPlayers(true)}
+            />
+            <IconButton
+              variant="nav"
+              icon={Save}
+              onClick={() => setSavePlayers(true)}
+              iconName="save"
+            />
+            <IconButton
+              variant="nav"
+              icon={IconAddPlayer}
+              iconName="addplayer"
+              onClick={() => withViewTransition(toggleOpenAddPlayer)}
+            />
+            <IconButton
+              variant="nav"
+              icon={Chart}
+              iconName="chart"
+              onClick={() => withViewTransition(toggleOpenCharts)}
+            />
+            <IconButton
+              variant="nav"
+              icon={isScoreHidden ? EyeClose : EyeOpen}
+              iconName="eyes"
+              onClick={toggleHideScore}
+            />
+            <IconButton
+              variant="nav"
+              icon={Undo}
+              iconName="undo"
+              onClick={() => {
+                if (!allowViewTransition) {
+                  undo();
+                  return;
+                }
+                withViewTransition(undo);
+                setAllowViewTransition(false);
+              }}
+              disabled={!isUndoPossible}
+            />
+            <IconButton
+              variant="nav"
+              icon={Redo}
+              iconName="redo"
+              onClick={() => {
+                if (!allowViewTransition) {
+                  redo();
+                  return;
+                }
+                withViewTransition(redo);
+                setAllowViewTransition(false);
+              }}
+              disabled={!isRedoPossible}
+            />
+          </ViewTransitionManager>
+        )}
       </nav>
     </Header>
+  );
+};
+
+const ViewTransitionManager = ({ children }: ContainerProps) => {
+  return (
+    <>
+      <style>
+        {`
+            ::view-transition-new(nav-vt) {
+              animation : slide-in-from-left 0.2s ease-out;
+            }
+
+            @keyframes slide-in-from-left {
+              0% {
+                transform: translateX(-200%);
+              }
+              100% {
+                transform: translateX(0);
+              }
+            }
+        `}
+      </style>
+      <div className={styles["view-transition-manager"]}>{children}</div>
+    </>
   );
 };
