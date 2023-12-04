d48ce3ffc1962b356eff89982fd8812a96df1b64
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-04 22:44:43 +0100

    *

--- src/components/utils/WindowEvents.tsx
+++ src/components/utils/WindowEvents.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { isProdEnv } from "@Utils";
 
 export const WindowEvents = () => {
+  console.log("WindowEvents");
   useEffect(() => {
     isProdEnv() &&
       addEventListener("beforeunload", (e) => {
