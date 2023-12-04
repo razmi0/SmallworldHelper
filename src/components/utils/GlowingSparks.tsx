d231e51c026295bca79f906ba2696903e8543fdc
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-04 16:33:16 +0100

    Add unused GlowingSparks comp

---
+++ src/components/utils/GlowingSparks.tsx
@@ -0,0 +1,299 @@
+// const GlowingSparks = () => {
+//   // impact-glow-1 impact-glow-2 impact-glow-3 static-impact-glow impact-glow circuit-comet serial-text sdk-text inner-circuit circuit-cover
+//   const classNamees = "circuit-glow ";
+//   return (
+//     <>
+//       <div style={{ width: "200px", height: "200px" }} className={"circuit-cover"}>
+//         <div className="inner-circuit "></div>
+//         <div className="circuit-comet ">
+//           <div className="comet-outer-glow absolute left-[-13px] top-[8px] h-[21px] w-[38px] rotate-[-36.366deg] rounded-[38px] bg-[#F8684B] opacity-50 blur-[21px]"></div>
+//         </div>
+//         <div className="impact-glow-1"></div>
+//         <div className="static-impact-glow "></div>
+//         <div className="impact-glow-3"></div>
+//       </div>
+//       <div
+//         style={{
+//           display: "flex",
+//           placeItems: "center",
+//           placeContent: "center",
+//           width: "100%",
+//           height: "100vh",
+//         }}
+//         // className={classNamees}
+//       >
+//         <svg
+//           width="1200"
+//           height="870"
+//           viewBox="-270 0 1200 550"
+//           fill="none"
+//           className="absolute left-[118px] top-[-274px]"
+//         >
+//           <path
+//             fillRule="evenodd"
+//             clipRule="evenodd"
+//             d="M925.488 -29.6667C769.276 -184.976 516.632 -184.778 360.662 -29.0726L-271 598.732C-99.6605 748.459 159.931 742.393 321.379 581.22L929.141 -25.5035L925.488 -29.6667Z"
+//             fill="none"
+//             stroke="currentColor"
+//             stroke-width="0.5"
+//           ></path>
+//           <defs>
+//             <clipPath id="parent-clip-path">
+//               <path
+//                 fillRule="evenodd"
+//                 clipRule="evenodd"
+//                 d="M925.488 -29.6667C769.276 -184.976 516.632 -184.778 360.662 -29.0726L-271 598.732C-99.6605 748.459 159.931 742.393 321.379 581.22L929.141 -25.5035L925.488 -29.6667Z"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 stroke-width="0.5"
+//               ></path>
+//             </clipPath>
+//           </defs>
+//           <g opacity="0.14" filter="url(#filter0_f_954_6668)" clip-path="url(#parent-clip-path)">
+//             <circle
+//               cx="556"
+//               cy="330"
+//               r="200"
+//               fill="#FBFBFC"
+//               className="translate-x-[69px] translate-y-[10px]"
+//             ></circle>
+//           </g>
+//           <defs>
+//             <filter
+//               id="filter0_f_954_6668"
+//               x="0"
+//               y="-226"
+//               width="1112"
+//               height="1112"
+//               filterUnits="userSpaceOnUse"
+//               color-interpolation-filters="sRGB"
+//             >
+//               <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+//               <feBlend
+//                 mode="normal"
+//                 in="SourceGraphic"
+//                 in2="BackgroundImageFix"
+//                 result="shape"
+//               ></feBlend>
+//               <feGaussianBlur
+//                 stdDeviation="178"
+//                 result="effect1_foregroundBlur_954_6668"
+//               ></feGaussianBlur>
+//             </filter>
+//           </defs>
+//         </svg>
+//         <svg
+//           width="1200"
+//           height="870"
+//           viewBox="-270 0 1200 550"
+//           fill="none"
+//           xmlns="http://www.w3.org/2000/svg"
+//           className="absolute left-[512px] top-[-19px]"
+//         >
+//           <path
+//             fill-rule="evenodd"
+//             clip-rule="evenodd"
+//             d="M925.488 -29.6667C769.276 -184.976 516.632 -184.778 360.662 -29.0726L-271 598.732C-99.6605 748.459 159.931 742.393 321.379 581.22L929.141 -25.5035L925.488 -29.6667Z"
+//             fill="none"
+//             stroke="currentColor"
+//             stroke-width="0.5"
+//           ></path>
+//           <defs>
+//             <clipPath id="parent-clip-path">
+//               <path
+//                 fill-rule="evenodd"
+//                 clip-rule="evenodd"
+//                 d="M925.488 -29.6667C769.276 -184.976 516.632 -184.778 360.662 -29.0726L-271 598.732C-99.6605 748.459 159.931 742.393 321.379 581.22L929.141 -25.5035L925.488 -29.6667Z"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 stroke-width="0.5"
+//               ></path>
+//             </clipPath>
+//           </defs>
+//           <path
+//             opacity="0.32"
+//             d="M631.838 117.739L631.839 117.739C787.71 -37.8671 1040.19 -38.0669 1196.31 117.139C1196.31 117.141 1196.31 117.143 1196.31 117.145L1199.8 121.119L592.203 727.677C430.913 888.693 171.611 894.816 0.366515 745.354L631.838 117.739Z"
+//             stroke="url(#paint0_radial_954_6677)"
+//             stroke-width="0.5"
+//             className="translate-x-[-271px] translate-y-[-147px]"
+//           ></path>
+//           <g opacity="0.08" filter="url(#filter0_f_954_6680)" clip-path="url(#parent-clip-path)">
+//             <circle
+//               cx="532.5"
+//               cy="399.635"
+//               r="200"
+//               fill="white"
+//               className="translate-x-[224px] translate-y-[-153px]"
+//             ></circle>
+//           </g>
+//           <defs>
+//             <filter
+//               id="filter0_f_954_6680"
+//               x="0.5"
+//               y="-132.365"
+//               width="1064"
+//               height="1064"
+//               filterUnits="userSpaceOnUse"
+//               color-interpolation-filters="sRGB"
+//             >
+//               <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+//               <feBlend
+//                 mode="normal"
+//                 in="SourceGraphic"
+//                 in2="BackgroundImageFix"
+//                 result="shape"
+//               ></feBlend>
+//               <feGaussianBlur
+//                 stdDeviation="166"
+//                 result="effect1_foregroundBlur_954_6680"
+//               ></feGaussianBlur>
+//             </filter>
+//             <radialGradient
+//               id="paint0_radial_954_6677"
+//               cx="0"
+//               cy="0"
+//               r="1"
+//               gradientUnits="userSpaceOnUse"
+//               gradientTransform="translate(1187 130) rotate(162.423) scale(168.885 237.689)"
+//             >
+//               <stop offset="0.0491201" stop-color="#FBFBFC"></stop>
+//               <stop offset="1" stop-color="#FBFBFC" stop-opacity="0"></stop>
+//             </radialGradient>
+//           </defs>
+//         </svg>
+//         <div className="relative z-10 h-[800px] overflow-hidden sm:z-0 sm:h-[447px]">
+//           <div className="absolute hidden h-[74px] w-[200px] translate-y-[-47px] sm:block travel-path left-[526px] top-[-19.5px] explosion-1">
+//             <div className="explosion-glow absolute left-[90px] top-[71px] h-[21px] w-[38px] rounded-[38px] bg-[#f8684b] opacity-50 blur-[28px]"></div>
+//             <div className="explosion-glow absolute left-[92px] top-[75px] h-[21px] w-[26px] rounded-[26px] bg-[#FE9D4E] opacity-50 blur-[14px]"></div>
+//             <div className="absolute translate-x-[105px] translate-y-[74px]">
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//               <div className="explosion-spark absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2"></div>
+//             </div>
+//           </div>
+//           <div className="travel-path left-[526px] top-[-19.5px] comet-1 absolute">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               width="32"
+//               height="32"
+//               viewBox="0 0 18 32"
+//               fill="none"
+//             >
+//               <path
+//                 d="M1.5 22L22.5 1"
+//                 stroke="url(#comet_path)"
+//                 stroke-width="1.5"
+//                 stroke-linecap="round"
+//               ></path>
+//               <g
+//                 opacity="0.8"
+//                 filter="url(#comet_blur)"
+//                 className="translate-x-[-6px] translate-y-[-6px]"
+//               >
+//                 <path
+//                   d="M7.5 28L28.5 7"
+//                   stroke="url(#comet_path)"
+//                   stroke-width="1.5"
+//                   stroke-linecap="round"
+//                 ></path>
+//               </g>
+//               <defs>
+//                 <linearGradient
+//                   id="comet_path"
+//                   x1="2.5"
+//                   y1="21"
+//                   x2="24"
+//                   y2="5.79346e-08"
+//                   gradientUnits="userSpaceOnUse"
+//                 >
+//                   <stop stop-color="#FFA24C"></stop>
+//                   <stop offset="0.362026" stop-color="#FF6A4C"></stop>
+//                   <stop offset="0.860711" stop-color="#FF6243" stop-opacity="0"></stop>
+//                 </linearGradient>
+//                 <filter
+//                   id="comet_blur"
+//                   x="0.75"
+//                   y="0.25"
+//                   width="34.5"
+//                   height="34.5"
+//                   filterUnits="userSpaceOnUse"
+//                   color-interpolation-filters="sRGB"
+//                 >
+//                   <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+//                   <feBlend
+//                     mode="normal"
+//                     in="SourceGraphic"
+//                     in2="BackgroundImageFix"
+//                     result="shape"
+//                   ></feBlend>
+//                   <feGaussianBlur
+//                     stdDeviation="3"
+//                     result="effect1_foregroundBlur_954_7697"
+//                   ></feGaussianBlur>
+//                 </filter>
+//               </defs>
+//             </svg>
+
+//             <div className="absolute left-[12.4px] top-[17px]">
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark-2"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//               <div className="absolute h-[1px] w-[1px] rounded-full bg-orange-glow-2 spark"></div>
+//             </div>
+//             <div className="comet-outer-glow absolute left-[-13px] top-[8px] h-[21px] w-[38px] rotate-[-36.366deg] rounded-[38px] bg-[#F8684B] opacity-50 blur-[21px]"></div>
+//           </div>
+//         </div>
+
+//         <div className="serial-text sdk-text">TEST</div>
+//       </div>
+//     </>
+//   );
+// };
