cdfa8005b9fb74050c8256226dad6722328fcb77
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-08 18:09:52 +0100

    Delete old .sh & duplicates

--- svg/build-icons.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Building SVG.... "
-tsc -t es2022 -m es2022 --moduleResolution node --outDir svg/dist --allowSyntheticDefaultImports true svg/build-icons.ts && node svg/dist/build-icons.js
-echo "Building SVG.... Done "
