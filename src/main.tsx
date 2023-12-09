c2edd0700cc4ee14a6f54e8f6473d9db78ab316f
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-09 11:11:57 +0100

    Refactor main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,33 @@
 import ReactDOM from "react-dom/client";
-// import { StrictMode } from "react";
 import { IntermediateProvider } from "@Context/useMid";
 import { NotificationProvider } from "@Context/useNotif.tsx";
 import { ThemeProvider } from "@Context/theme/useTheme.tsx";
+import { useWindowEvents } from "@/hooks/useWindowEvents.ts";
 import App from "./App.tsx";
 import "@Css";
-import { WindowEvents } from "@Components/WindowEvents.tsx";
 import { MouseToolTipContext } from "@Context/MouseToolTipContext.tsx";
+
+const Main = () => {
+  useWindowEvents();
+  return (
+    <>
+      <ThemeProvider>
+        <NotificationProvider>
+          <IntermediateProvider>
+            <MouseToolTipContext.Provider
+              value={{ portalTarget: document.getElementById("portal-target") as HTMLDivElement }}
+            >
+              <App />
+            </MouseToolTipContext.Provider>
+          </IntermediateProvider>
+        </NotificationProvider>
+      </ThemeProvider>
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <WindowEvents />
-    <ThemeProvider>
-      <NotificationProvider>
-        <IntermediateProvider>
-          <MouseToolTipContext.Provider
-            value={{ portalTarget: document.getElementById("portal-target") as HTMLDivElement }}
-          >
-            <App />
-          </MouseToolTipContext.Provider>
-        </IntermediateProvider>
-      </NotificationProvider>
-    </ThemeProvider>
+    <Main />
   </>
 );
