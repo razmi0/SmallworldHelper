a12f2bef72530fd5871d9d971f6ca12f1b447938
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-09 21:10:21 +0100

    Change TIME_BEFORE_RESET_FOCUS

--- src/utils/charts/options.ts
+++ src/utils/charts/options.ts
@@ -1,6 +1,6 @@
 import { ChartOptions } from "chart.js";
 
-export const TIME_BEFORE_RESET_FOCUS = 40000 as const;
+export const TIME_BEFORE_RESET_FOCUS = 5000 as const;
 
 // declare module "chart.js" {
 //   interface TooltipPositionerMap {
