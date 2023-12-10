b13a8b7cf89d59a52a4ba9f04203331c683d358f
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-10 13:31:30 +0100

    Fix HMR bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,26 @@
 import ReactDOM from "react-dom/client";
 import { StorageProvider } from "@Context/useStorage";
 import { NotificationProvider } from "@Context/useNotif.tsx";
-import { ThemeProvider } from "@Context/theme/useTheme.tsx";
 import { MouseToolTipContext } from "@Context/MouseToolTipContext.tsx";
-import { useWindowEvents } from "@/hooks/useWindowEvents.ts";
 import App from "./App.tsx";
 import "@Css";
-import { isProdEnv } from "./utils/utils.ts";
 
-const Main = () => {
-  useWindowEvents(isProdEnv());
-  return (
-    <>
-      <ThemeProvider>
-        <NotificationProvider>
-          <StorageProvider>
-            <MouseToolTipContext.Provider
-              value={{ portalTarget: document.getElementById("portal-target") as HTMLDivElement }}
-            >
-              <App />
-            </MouseToolTipContext.Provider>
-          </StorageProvider>
-        </NotificationProvider>
-      </ThemeProvider>
-    </>
-  );
-};
+// import { ThemeProvider } from "@Context/theme/useTheme.tsx";
+// </ThemeProvider>
+// <ThemeProvider>
+
+// import { useWindowEvents } from "@/hooks/useWindowEvents.ts";
+// import { isProdEnv } from "./utils/utils.ts";
+// useWindowEvents(isProdEnv());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <Main />
-  </>
+  <NotificationProvider>
+    <StorageProvider>
+      <MouseToolTipContext.Provider
+        value={{ portalTarget: document.getElementById("portal-target") as HTMLDivElement }}
+      >
+        <App />
+      </MouseToolTipContext.Provider>
+    </StorageProvider>
+  </NotificationProvider>
 );
