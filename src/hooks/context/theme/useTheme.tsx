caea148a450573b769d7919bb0c0e9f7840ce450
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-10 13:36:39 +0100

    Commented out useTheme

--- src/hooks/context/theme/useTheme.tsx
+++ src/hooks/context/theme/useTheme.tsx
@@ -1,65 +1,65 @@
-import { createContext, useContext, ReactNode, useReducer, useCallback } from "react";
-import { getFromLocalStorage, saveToLocalStorage } from "@Utils/utils";
-import { cssModules } from "@Components/styles";
+// import { createContext, useContext, ReactNode, useReducer, useCallback } from "react";
+// import { getFromLocalStorage, saveToLocalStorage } from "@Utils/utils";
+// import { cssModules } from "@Components/styles";
 
-type ThemeState = "light" | "dark";
-type ThemeContextType = "light" | "dark";
-type ThemeDispatchContextType = () => void;
+// type ThemeState = "light" | "dark";
+// type ThemeContextType = "light" | "dark";
+// type ThemeDispatchContextType = () => void;
 
-const ThemeContext = createContext<ThemeContextType | null>(null);
-const ThemeDispatchContext = createContext<ThemeDispatchContextType | null>(null);
+// const ThemeContext = createContext<ThemeContextType | null>(null);
+// const ThemeDispatchContext = createContext<ThemeDispatchContextType | null>(null);
 
-const themeReducer = (state: ThemeState, action = "SWITCH"): ThemeState => {
-  switch (action) {
-    case "SWITCH": {
-      const newState = state === "dark" ? "light" : "dark";
-      saveToLocalStorage(`__smallWorld_th`, newState);
-      setStyles(newState);
-      return newState;
-    }
-    default:
-      return state;
-  }
-};
+// const themeReducer = (state: ThemeState, action = "SWITCH"): ThemeState => {
+//   switch (action) {
+//     case "SWITCH": {
+//       const newState = state === "dark" ? "light" : "dark";
+//       saveToLocalStorage(`__smallWorld_th`, newState);
+//       setStyles(newState);
+//       return newState;
+//     }
+//     default:
+//       return state;
+//   }
+// };
 
-export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, switchTheme] = useReducer(themeReducer, initTheme());
+// export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+//   const [theme, switchTheme] = useReducer(themeReducer, initTheme());
 
-  return (
-    <ThemeContext.Provider value={theme}>
-      <ThemeDispatchContext.Provider value={switchTheme}>{children}</ThemeDispatchContext.Provider>
-    </ThemeContext.Provider>
-  );
-};
+//   return (
+//     <ThemeContext.Provider value={theme}>
+//       <ThemeDispatchContext.Provider value={switchTheme}>{children}</ThemeDispatchContext.Provider>
+//     </ThemeContext.Provider>
+//   );
+// };
 
-export const useTheme = () => {
-  const theme = useContext(ThemeContext);
-  if (theme === null) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-  return { theme };
-};
+// export const useTheme = () => {
+//   const theme = useContext(ThemeContext);
+//   if (theme === null) {
+//     throw new Error("useTheme must be used within a ThemeProvider");
+//   }
+//   return { theme };
+// };
 
-export const useSwitchTheme = () => {
-  const dispatch = useContext(ThemeDispatchContext);
-  if (dispatch === null) {
-    throw new Error("useSwitchTheme must be used within a ThemeProvider");
-  }
-  const switchTheme = useCallback(() => dispatch!(), [dispatch]);
-  return { switchTheme };
-};
+// export const useSwitchTheme = () => {
+//   const dispatch = useContext(ThemeDispatchContext);
+//   if (dispatch === null) {
+//     throw new Error("useSwitchTheme must be used within a ThemeProvider");
+//   }
+//   const switchTheme = useCallback(() => dispatch!(), [dispatch]);
+//   return { switchTheme };
+// };
 
-const initTheme = () => {
-  const systemMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "dark";
-  const localMode = getFromLocalStorage<ThemeState | false>("__smallWorld_th", false);
+// const initTheme = () => {
+//   const systemMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "dark";
+//   const localMode = getFromLocalStorage<ThemeState | false>("__smallWorld_th", false);
 
-  setStyles(localMode.stored || systemMode);
-  return localMode.stored || systemMode;
-};
+//   setStyles(localMode.stored || systemMode);
+//   return localMode.stored || systemMode;
+// };
 
-const body = document.querySelector("body")!;
-const setStyles = (theme: ThemeState) => {
-  const isDark = theme === "dark";
+// const body = document.querySelector("body")!;
+// const setStyles = (theme: ThemeState) => {
+//   const isDark = theme === "dark";
 
-  body.className = `${isDark ? cssModules.theme["dark-body"] : cssModules.theme["light-body"]}`;
-};
+//   body.className = `${isDark ? cssModules.theme["dark-body"] : cssModules.theme["light-body"]}`;
+// };
